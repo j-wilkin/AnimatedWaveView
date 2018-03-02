79ecf486e8481a8ad0ed51434f45c35cd73bbc5d
Jason Wilkin <jason.wilkin@nike.com>
2018-03-02 14:55:46 -0500

    Initial Commit

---
+++ AnimatedWaveViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimatedWaveViewExample
+//
+//  Created by Wilkin, Jason on 3/2/18.
+//  Copyright © 2018 Nike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
